c5f6da74f16a2ccd9830a269bac949b0cd79c630
snoppy <1840607982@qq.com>
2016-07-31 20:49:52 +0800

    Add files via upload

---
+++ a4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0,b = 0,result = 0;
+	cout<<"Please input two numbers£º"<<endl;
+	cin>>a>>b; 
+	result = a+b;
+	cout<<"result is£º"<<result<<endl;
+	return 0; 
+}
